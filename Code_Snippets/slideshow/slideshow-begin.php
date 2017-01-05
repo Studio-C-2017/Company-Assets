b38cbf1d949ac928d76c747521a30afe939e8ba9
Addison C. Quijano Sorca <addison.sorca@gmail.com>
2017-01-04 21:23:01 -0800

    Restructure PHP

--- Code_Snippets/slideshow/slideshow-begin.php
+++ Code_Snippets/slideshow/slideshow-begin.php
@@ -1,2 +1,26 @@
-<div id="slideshow">
-    <div id="carousel">
+
+<?php
+    global $number_of_slides;
+    $slides_info = array();
+    // needs $category, $sku, $name, $description
+?>
+
+<?php
+    class slide {
+        function __construct($url,$img,$name,$description) {
+            $this->url = $url;
+            $this->img = $img;
+            $this->name = $name;
+            $this->description = $description;
+        }
+    }
+?>
+
+    <?php
+        function create_slide($url, $img, $name, $description) {
+            global $slides_info;
+            $slides_info[] = new slide($url, $img, $name, $description);
+            global $number_of_slides;
+            $number_of_slides++;
+        }
+    ?>
--- Code_Snippets/slideshow/slideshow.php
+++ Code_Snippets/slideshow/slideshow.php
@@ -1,55 +1,42 @@
 
-<?php
-    global $number_of_slides;
-    // needs $category, $sku, $name, $description
-?>
 
-<?php function begin_slideshow() { ?>
 <div id='slideshow'>
     <div id='carousel'>
-<?php } // End begin_slideshow() ?>
-        
-    <?php function create_slide($url, $img, $name, $description) {
-        global $number_of_slides;  
-        $number_of_slides++;
-    ?>
+
+    <?php foreach ($slides_info as $current_slide): ?>
         <div class="slide">
 
-            <a href="<?php echo $url; ?>">
-                <img src="<?php echo $img; ?>"/>
+            <a href="<?php echo $current_slide->url; ?>">
+                <img src="<?php echo $current_slide->img; ?>"/>
             </a>
 
             <div class="slide-info">
-                <a href="<?php echo $url; ?>">
+                <a href="<?php echo $current_slide->url; ?>">
                     <h2 class="slide-title">
-                        <?php echo $name; ?>
+                        <?php echo $current_slide->name; ?>
                     </h2>
                     <span class="slide-description">
-                        <?php echo $description; ?>
+                        <?php echo $current_slide->description; ?>
                     </span>
                 </a>
             </div> <!-- END slide-info -->
 
         </div> <!-- END slide -->
-<?php } // End create_slide() ?>
+    <?php endforeach; ?>
 
-<?php
-    function end_slideshow() {
-?>
-        </div> <!--END carousel-->
 
-        <div id="slide-icon-container">
-            <?php
-                $number_of_slides = 0;
-                while ($number_of_slides > 0):
-            ?>
-                    <button class="slide-icon"></button>
-                    <?php $number_of_slides--; ?>
-            <?php endwhile; ?>
-        </div> <!-- slide-icon-container -->
+    </div> <!--END carousel-->
 
-        <button id="left-arrow"></button>
-        <button id="right-arrow"></button>
-    </div> <!-- END slideshow -->
-<?php }
-?>
+    <div id="slide-icon-container">
+        <?php
+            $number_of_slides = 0;
+            while ($number_of_slides > 0):
+        ?>
+                <button class="slide-icon"></button>
+                <?php $number_of_slides--; ?>
+        <?php endwhile; ?>
+    </div> <!-- slide-icon-container -->
+
+    <button id="left-arrow"></button>
+    <button id="right-arrow"></button>
+</div> <!-- END slideshow -->
