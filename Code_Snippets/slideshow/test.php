189169dbc89627edd06bda59a429dae343755394
Addison C. Quijano Sorca <addison.sorca@gmail.com>
2017-01-10 22:27:46 -0800

    Fix links to includes files in test.php

--- Code_Snippets/slideshow/test.php
+++ Code_Snippets/slideshow/test.php
@@ -1,13 +1,12 @@
 
 
 <link rel="stylesheet" type="text/css" href="assets/css/slideshow.css">
-<?php include('slide.php'); ?>
+<?php include('includes/slide.php'); ?>
 <?php
 	new slide('example.com', 'http://static.boredpanda.com/blog/wp-content/uploads/2014/03/amazing-fox-photos-3.jpg', 'Stuff', 'Another Thing');
     new slide('example.com', 'http://static.boredpanda.com/blog/wp-content/uploads/2016/05/cute-baby-foxes-cubs-17-574436be67482__880.jpg', 'Stuff', 'Another Thing');
     new slide('example.com', 'http://www.demilked.com/magazine/wp-content/uploads/2014/04/beautiful-foxes-wildlife-photography-9.jpg', 'Stuff', 'Another Thing');
     new slide('example.com', 'https://s-media-cache-ak0.pinimg.com/originals/a1/b4/91/a1b491dca722c54ddf63d9bbabc43ed4.jpg', 'Stuff', 'Another Thing');
-	include('slideshow.php');
+	include('includes/slideshow.php');
 ?>
-<?php //include('slideshow-end.php'); ?>
 <script src="assets/js/slideshow-script.js"></script>
