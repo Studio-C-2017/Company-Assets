78b05c7ed365cfc98ffffc1b23a8120bd34741f4
Addison C. Quijano Sorca <addison.sorca@gmail.com>
2017-01-11 09:47:35 -0800

    Fix array declaration

--- Code_Snippets/slideshow/includes/slide.php
+++ Code_Snippets/slideshow/includes/slide.php
@@ -1,16 +1,13 @@
 <?php
+    $slides_info = array();
     class slide {
         function __construct($url,$img,$name,$description) {
             $this->url = $url;
             $this->img = $img;
             $this->name = $name;
             $this->description = $description;
-			global $slides_info;
-			if (!isset($slides_info)) {
-				$slides_info = array();
-			} else {
-            	$slides_info[] = $this;
-			}
+            global $slides_info;
+            $slides_info[] = $this;
         }
     }
 ?>
